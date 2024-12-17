fca8c4462291f959ac7e9774cb9707a89dfeaf08
EthanElms <ethanelms@seattleacademy.org>
2024-12-17 21:37:57 +0000

    Added more questions

--- Projects/project2.py
+++ Projects/project2.py
@@ -1,29 +1,32 @@
 Score = [0,0,0,0,0]
 
-print("this is a test to determine how much you know about space")
+print("this is a test to determine how much you know about space\n")
 
-Answer = input("what is prograde, A) direction of current orbit, B) Opposite direction of current orbit, C) Down Relative to nearest gravity \n")
+Answer = input("what is prograde \nA) direction of current orbit \nB) Opposite direction of current orbit \nC) Down Relative to nearest gravity \n")
 Answer = Answer.lower()
 
 if (Answer == "a"):
     Score[0] += 1
 elif (Answer != "b" or Answer != "c"):
-    print()
-else:
+    print("Invalid Answer marked as incorrect")
+    
+
+Answer = input("How many planets are there in the solar system \nA) 5 \nB) 9 \nC) 8")
+Answer = Answer.lower()
+
+if (Answer == "b"):
+    Score[1] += 1
+elif (Answer != "a" or Answer != "c"):
     print("Invalid Answer marked as incorrect")
 
 
-Answer = input("what is prograde, A) direction of current orbit ")
+Answer = input("DeltaV \nA) 5 \nB) 9 \nC) 8")
 Answer = Answer.lower()
 
 if (Answer == "a"):
-    Score[1] += 1
+    Score[2] += 1
 elif (Answer != "b" or Answer != "c"):
-    print()
-else:
     print("Invalid Answer marked as incorrect")
-
-
 
 
 print("results: \n")
