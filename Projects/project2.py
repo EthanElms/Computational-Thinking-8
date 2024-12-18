18f8dd9a99e24634a1e3f44023d0816c2376ee8a
EthanElms <ethanelms@seattleacademy.org>
2024-12-18 17:29:19 +0000

    Almost done with questions

--- Projects/project2.py
+++ Projects/project2.py
@@ -1,7 +1,10 @@
+#begining
+
 Score = [0,0,0,0,0]
 
 print("this is a test to determine how much you know about space\n")
 
+#Middle
 Answer = input("what is prograde \nA) direction of current orbit \nB) Opposite direction of current orbit \nC) Down Relative to nearest gravity \n")
 Answer = Answer.lower()
 
@@ -14,13 +17,13 @@ elif (Answer != "b" or Answer != "c"):
 Answer = input("How many planets are there in the solar system \nA) 5 \nB) 9 \nC) 8")
 Answer = Answer.lower()
 
-if (Answer == "b"):
+if (Answer == "c"):
     Score[1] += 1
-elif (Answer != "a" or Answer != "c"):
+elif (Answer != "a" or Answer != "b"):
     print("Invalid Answer marked as incorrect")
 
 
-Answer = input("DeltaV \nA) 5 \nB) 9 \nC) 8")
+Answer = input("What is DeltaV \nA) Change in velocity \nB) Current velocity \nC) Mass\n")
 Answer = Answer.lower()
 
 if (Answer == "a"):
@@ -28,7 +31,24 @@ if (Answer == "a"):
 elif (Answer != "b" or Answer != "c"):
     print("Invalid Answer marked as incorrect")
 
+Answer = input("Most effective place to do a prograde burn \nA) Midpoint \nB) apoapsis \nC) Periapsis\n")
+Answer = Answer.lower()
 
+if (Answer == "c"):
+    Score[3] += 1
+elif (Answer != "b" or Answer != "a"):
+    print("Invalid Answer marked as incorrect")
+
+
+Answer = input("Roughly how many stars are there in the galaxy \nA) 100 Million \nB) Current velocity \nC) Mass\n")
+Answer = Answer.lower()
+
+if (Answer == "b"):
+    Score[4] += 1
+elif (Answer != "a" or Answer != "c"):
+    print("Invalid Answer marked as incorrect")
+
+#end
 print("results: \n")
 for x in Score:
     if (x == 1):
