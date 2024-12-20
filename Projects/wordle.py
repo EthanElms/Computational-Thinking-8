3ea0d0b67d41e3b4db594ede4577948647996246
EthanElms <ethanelms@seattleacademy.org>
2024-12-20 20:00:55 +0000

    not doing this anymore

--- Projects/wordle.py
+++ Projects/wordle.py
@@ -1,5 +1,74 @@
-import codesters
-from codesters import StageClass
-stage = StageClass()
+import random
+#WARINING NOT GOOD DONT LOOK
 
 
+#WARINING NOT GOOD DONT LOOK
+
+
+
+#WARINING NOT GOOD DONT LOOK
+# Pick a word at random
+word_list = ["loopy","heart","audio","laugh","trial","slant","queer","happy","house","sleep","earth"]
+hidden_word = random.choice(word_list)
+
+# Repeat for 6 guesses
+for i in range(6):
+    # Guess a word
+    guess_word = input().strip().lower()
+    if ( guess_word.isnumeric):
+        print("invalid")
+        
+    if (len(guess_word) > 5 or len(guess_word) < 5):
+        print("invalid")
+    output = ""
+
+    # First letter (in python, counting starts at 0 not 1)
+    if guess_word[0] == hidden_word[0]:
+        output += "🟩"
+    elif guess_word[0] in hidden_word:
+        output += "🟨"
+    else:
+        output += "⬛"
+    
+
+    # Second letter (in python, counting starts at 0 not 1)
+    if guess_word[1] == hidden_word[1]:
+        output += "🟩"
+    elif guess_word[1] in hidden_word:
+        output += "🟨"
+    else:
+        output += "⬛"
+
+    # Third letter (in python, counting starts at 0 not 1)
+    if guess_word[2] == hidden_word[2]:
+        output += "🟩"
+    elif guess_word[2] in hidden_word:
+        output += "🟨"
+    else:
+        output += "⬛"   
+
+    # Fourth letter (in python, counting starts at 0 not 1)
+    if guess_word[3] == hidden_word[3]:
+        output += "🟩"
+    elif guess_word[3] in hidden_word:
+        output += "🟨"
+    else:
+        output += "⬛"
+
+    # Fifth letter (in python, counting starts at 0 not 1)
+    if guess_word[4] == hidden_word[4]:
+        output += "🟩"
+    elif guess_word[4] in hidden_word:
+        output += "🟨"
+    else:
+        output += "⬛"
+
+
+
+    # Result
+    print(output)
+    if output == "🟩🟩🟩🟩🟩":
+        print("You win")
+        break
+
+print(f"You used {i+1} guesses")
