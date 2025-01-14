416a824835f9e975436bee3ece3d21311e4bfb15
EthanElms <ethanelms@seattleacademy.org>
2025-01-14 17:08:05 +0000

    p3

--- Projects/project3.py
+++ Projects/project3.py
@@ -1,47 +1,27 @@
 import turtle
 import random
 
+#t1 setup
+x = 10
 
-t1 = turtle.Turtle()
-t1.speed(0)
-t1.penup()
-t1.goto(0, 0)
-t1.color("red")
-t1.pendown()
+class tsetup:
+    def __init__(self, x):
+        t1 = turtle.Turtle()
+        t1.speed(4)
+        t1.penup()
+        t1.goto(0, -200)
+        t1.color("black")
+        t1.pendown()
+t1 = tsetup(x)
+#screen color
+#turtle.Screen().bgcolor("white")
 
-t2 = turtle.Turtle()
-t2.speed(0)
-t2.penup()
-t2.goto(0, 0)
-t2.color("white")
-t2.right(20)
-t2.pendown()
 
-turtle.Screen().bgcolor("black")
+#drawing
+for x in range(6):
+    for x in range(6):
+        #t1.forward(100)
+        print()
 
-while(True):
-    t1angle = random.randrange(10,60)
-    t2angle = random.randrange(10,60)
-
-    t1distance = random.randrange(10,60)
-    t2distance = random.randrange(10,60)
-
-    t1dir = random.randrange(1,2)
-    t2dir = random.randrange(1,2)
-    t1.pendown()
-    
-    if(t1dir==1):
-        t1.right(t1angle)
-    else:
-        t1.left(t1angle)
-    t1.forward(t1distance)
-
-    t2.pendown()
-    if(t2dir==1):
-        t2.right(t2angle)
-    else:
-        t2.left(t2angle)
-    t2.forward(t2distance)
-    
 
 turtle.exitonclick()
